87304dc863bcaffa5a00dd86a580f20432fab1a3
nanayyayaa <jiejueejuju@gmail.com>
2026-03-31 11:34:28 +0700

    commit 3

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -9,5 +9,9 @@ int pilihan;
 void tampilkanMenu() {
     cout << "\n====================================\n";
     cout << "       MENU CEK BILANGAN\n";
-
+    cout << "===================================\n";
+    cout << "1. Cek Bilangan Prima\n";
+    cout << "2. Cek Bilangan Fibonacci\n";
+    cout << "0. Keluar\n";
+    cout << "Maukkan pilihan: ";
 }
