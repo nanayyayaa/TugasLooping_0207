99d10e758e4d3cd06685d257d6c8e6509c0e0f16
nanayyayaa <jiejueejuju@gmail.com>
2026-03-31 11:31:26 +0700

    commit 2

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -4,3 +4,10 @@ using namespace std;
 //variabel global
 int angka;
 int pilihan;
+
+//Function untuk menampilkan menu
+void tampilkanMenu() {
+    cout << "\n====================================\n";
+    cout << "       MENU CEK BILANGAN\n";
+
+}
